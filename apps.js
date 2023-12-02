5f9b143cc0c758a2046ff58b32aea7ea08020884
Troy M <legendarytroy20@gmail.com>
2023-12-01 21:41:18 -0500

    docs: Add operate function

--- apps.js
+++ apps.js
@@ -1,3 +1,4 @@
+//Basic Calculator Functions
 const add = (a, b) => {
     return a + b;
 };
@@ -13,3 +14,24 @@ const multiply =  (a, b) => {
 const divide = () => {
     return a/b;
 }
+
+//Variables for Calculator Operation
+let firstNumber;
+let operator;
+let secondNumber;
+
+//Operate Function
+const operate = (firstNumber, secondNumber, operator) => {
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+        default:
+            return "Invalid operator";
+    };
+};
